c4be5382019305213c68300c74a0959f23dcd2dc
chathura_jayashan <chathurachamod88@gmail.com>
2025-11-08 22:34:45 +0530

    f

--- src/Components/SampleDetails/SampleDetails.jsx
+++ src/Components/SampleDetails/SampleDetails.jsx
@@ -24,7 +24,7 @@ export default function SampleDetails() {
       }
 
       try {
-        const res = await axios.get(`http://localhost:5000/samples/public/${id}`);
+        const res = await axios.get(`https://hay-card-back-end.vercel.app/api/samples/public/${id}`);
         setSample(res.data);
       } catch (err) {
         console.error("Error fetching sample:", err);
